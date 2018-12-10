e6c8cb87583fae3f5bcbaea8cb12dba4b12c3113
EnricoMuraro <enrico_97@hotmail.it>
2018-12-10 20:54:59 +0100

    creato MenuItem. close #1

---
+++ src/main/java/it/unipd/tos/model/MenuItem.java
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////
+// Enrico Muraro 1143775
+////////////////////////////////////////////////////////////////////
+
+
+package it.unipd.tos.model;
+
+public class MenuItem {
+
+    public enum itemType {
+        PIZZA, PRIMO
+    }
+
+    private itemType type;
+    private String name;
+    private double price;
+
+    public MenuItem(itemType type, String name, double price)
+    {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    public itemType getType(){
+        return type;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+}
