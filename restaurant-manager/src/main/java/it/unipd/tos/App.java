48ebc6ff6590a1a55f0ca00edefb3a86658946c4
EnricoMuraro <enrico_97@hotmail.it>
2018-12-10 19:37:10 +0100

    creazione progetto e checkstyle. close #4

---
+++ restaurant-manager/src/main/java/it/unipd/tos/App.java
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////
+// Enrico Muraro 1143775
+////////////////////////////////////////////////////////////////////
+
+
+package it.unipd.tos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
